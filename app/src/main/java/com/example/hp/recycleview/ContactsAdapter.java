d6514ed461cebd7a8db341ef858aa8853439c163
Ardabilli1 <billya749@gmail.com>
2019-02-27 22:47:45 +0700

    creating adapter

---
+++ app/src/main/java/com/example/hp/recycleview/ContactsAdapter.java
@@ -0,0 +1,79 @@
+package com.example.hp.recycleview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ContactsAdapter extends
+        RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
+    private List<Contact> mContacts;
+    public TextView nameTextView;
+    public Button messageButton;
+    public ContactsAdapter(List<Contact> contacts) {
+        mContacts = contacts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactsAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Contact contact = mContacts.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(contact.getName());
+        Button button = viewHolder.messageButton;
+        button.setText(contact.isOnline() ? "Hidup" : "Offline");
+        button.setEnabled(contact.isOnline());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+            // Your holder should contain a member variable
+            // for any view that will be set as you render a row
+            public TextView nameTextView;
+            public Button messageButton;
+            private List<Contact> mContacts;
+
+
+
+
+
+            // We also create a constructor that accepts the entire item row
+            // and does the view lookups to find each subview
+            public ViewHolder(View itemView) {
+                // Stores the itemView in a public final member variable that can be used
+                // to access the context from any ViewHolder instance.
+                super(itemView);
+
+                nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+                messageButton = (Button) itemView.findViewById(R.id.message_button);
+            }
+        }
+    }
+}
